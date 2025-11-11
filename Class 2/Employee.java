7fe4234ef0514c137165e81cecb5ebe47b77ab6a
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-11-11 15:55:48 +0530

    java

--- Class 2/Employee.java
+++ Class 2/Employee.java
@@ -1,4 +1,4 @@
-public class Employee {
+ public class Test {
 
     // static int 9eid = 101;     identifier should not start with digits
     // static int for = 101;         identifier should not use keywords/ Reserved words
