d27c6eaf362273d57e792c73a430b9e25e7c3eff
kalaivanan564 <kalaivananeswaran564@gmail.com>
2025-12-01 12:08:46 +0530

    Inheritance

---
+++ Class 17/Inheritance/Single.java
@@ -0,0 +1,21 @@
+
+class Parent {
+    public void m1(){
+        System.out.println("Parent calss - m1 method");
+    }
+
+}
+
+class Child extends Parent{
+    public void m2(){
+        System.out.println("Child class m2");
+    }
+}
+
+class Test{
+    public static void main(String[] args) {
+        Child c1 = new Child();
+        c1.m1();
+        c1.m2();
+    }
+}
